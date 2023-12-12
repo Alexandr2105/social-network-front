9fe8cf62b974ad54ca792073233aa71ca54c6cd5
Alexandr2105 <5030553@gmail.com>
2023-12-12 23:25:04 +0300

    del export in renderApp

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-export const renderApp = (state) => {
+const renderApp = (state) => {
     root.render(
         <BrowserRouter>
             <App state={state} dispatch={store.dispatch.bind(store)}/>
